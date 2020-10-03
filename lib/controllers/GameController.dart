aecf3a2ead99dae022a057534eb4ae618b31f876
Lê Xuân Vinh <zpring.vinh@gmail.com>
2020-10-03 10:06:00 +0700

    coumnCheck

--- lib/controllers/GameController.dart
+++ lib/controllers/GameController.dart
@@ -23,7 +23,8 @@ class GameController extends GetxController {
       final database = FirebaseDatabase.instance.reference();
       database.child('Room/$roomId').set({
         'player': {
-          'player1': {'uid': uid, 'username': username},
+          'player1': {'uid': uid, 'username': username, 'score': 0},
+          'player2': {'score': 0},
         },
         'gameboard': cells,
         'switchTurn': 0,
@@ -39,21 +40,74 @@ class GameController extends GetxController {
   void updateCellState(Map<dynamic, dynamic> roomData, int index) {
     if (roomData['gameboard'][index]['state'] == 0 &&
         roomData['isPlaying'] == true) {
+          
       if (roomData['switchTurn'] % 2 == 0) {
         
         database.child('Room/$roomId/').update({'switchTurn': roomData['switchTurn'] + 1}).then((value) => {
-          database.child('Room/$roomId/gameboard/$index/').update({'state': 1})
+          database.child('Room/$roomId/gameboard/$index/').update({'state': 1}).then((value) => {
+            columnCheck(roomData, index, roomData['gameboard'][index]['column'], roomData['gameboard'][index]['row'], 1)
+          })
         });
 
       } else {
 
         database.child('Room/$roomId/').update({'switchTurn': roomData['switchTurn'] + 1}).then((value) => {
-          database.child('Room/$roomId/gameboard/$index/').update({'state': 2})
+          database.child('Room/$roomId/gameboard/$index/').update({'state': 2}).then((value) => {
+            columnCheck(roomData, index, roomData['gameboard'][index]['column'], roomData['gameboard'][index]['row'], 2)
+          })
         });
 
       }
     }
   }
+
+
+  void columnCheck(Map<dynamic, dynamic> roomData, int index, int column, int row, int value) {
+    
+    int start = row;
+    int end = row;
+    int startIndex = index;
+    int count = 0;
+
+    if(start - 4 <= 0) {
+
+      for(int i = row; i > 0; i--) {
+
+        startIndex = startIndex - 1;
+      } 
+
+    } else {
+      startIndex = startIndex - 4;
+    }
+
+    start - 4 < 0 ? start = 0 : start = start - 4;
+    end + 4 > 19 ? end = 19 : end = end + 4;
+
+    for(int i = start; i <= end; i ++) {
+
+      if(roomData['gameboard'][startIndex]['state'] == value) {
+
+        count = count + 1;
+
+        if(count == 4) {
+          database.child('Room/$roomId/player/player$value/').update({
+            'score': roomData['player']['player$value']['score'] + 1
+          });
+        }
+
+      } else {
+
+        count = 0;
+
+      }
+
+      startIndex = startIndex + 1;
+
+    }
+
+  }
+
+
 }
 
 void showSnackBar(String errorMessage) {
--- lib/screens/gameboard.dart
+++ lib/screens/gameboard.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:caro/controllers/GameController.dart';
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
@@ -32,20 +34,68 @@ class _GameBoardState extends State<GameBoard> {
 
   Widget build(BuildContext context) {
 
-    int as;
-
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
     // TODO: implement build
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        title: Text('$roomId', style: TextStyle(color: Colors.black),),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
       body: SafeArea(
         child: Column(
           children: [
             Container(
-              child: SizedBox(
-                height: 200.0,
-              ),
+              padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0, bottom: 24.0),
+              child: StreamBuilder(
+                stream: Get.find<GameController>().database.child('Room/$roomId/player').onValue,
+                builder: (context, snapshot) {
+
+                  if(!snapshot.hasData) {
+                    return Container();
+                  }
+
+                  Map<dynamic, dynamic> players = snapshot.data.snapshot.value;
+                  print(players);
+
+                  return Row(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset('assets/images/icons8-multiply-96.png', scale: 2.0,),
+                          SizedBox(height: 15.0),
+                          Text(players['player1']['username'], style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Text(players['player1']['score'].toString(), style: TextStyle(fontSize: 56.0, fontWeight: FontWeight.bold, color: Colors.red),),
+                          Container(
+                            height: 25.0,
+                            width: 1.0,
+                            margin: EdgeInsets.only(left: 25.0, right: 25.0),
+                            color: Colors.black,
+                          ),
+                          Text(players['player2']['score'].toString(), style: TextStyle(fontSize: 56.0, fontWeight: FontWeight.bold, color: Colors.blue)),
+                        ],
+                      ),
+                      Spacer(),
+                      Column(
+                        children: [
+                          Image.asset('assets/images/icons8-circle-96.png', scale: 2.0,),
+                          SizedBox(height: 15.0),
+                          Text(players['player2']['username'] == null ? 'NULL' : players['player2']['username'], style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),)
+                        ],
+                      )
+                    ],
+                  );
+                }
+              )
             ),
             Expanded(
               child: Container(
@@ -64,6 +114,7 @@ class _GameBoardState extends State<GameBoard> {
                           return Container();
                         }
 
+
                         Map<dynamic, dynamic> roomData = snapshot.data.snapshot.value;
                         List<dynamic> cells = roomData['gameboard'];
                         // print(roomData['player']['player2']);
@@ -76,48 +127,14 @@ class _GameBoardState extends State<GameBoard> {
                             return InkWell(
                               onTap: () async {
 
-
                                 Get.find<GameController>().updateCellState(roomData, index);
-                                // if(roomData['turn'] % 2 == 0) {
-
-                                //   Get.find<GameController>().database.child('Room/$roomId/gameboard/$index/').update({
-                                //     'state': 1
-                                //   }).then((value) => {
-                                    
-                                //     Get.find<GameController>().database.child('Room/$roomId/').update({
-                                //       'turn': roomData['turn'] + 1
-                                //     })
-
-                                //   });
-
-                                // } else {
-
-                                //   Get.find<GameController>().database.child('Room/$roomId/gameboard/$index/').update({
-                                //     'state': 2
-                                //   }).then((value) => {
-                                    
-                                //     Get.find<GameController>().database.child('Room/$roomId/').update({
-                                //       'turn': roomData['turn'] + 1
-                                //     })
-
-                                //   });
-
-                                // }
-
-
-                                // int x = (index % 20);
-                                // int y = (index / 20).floor();
-                                // print(x);
-                                // print(y);
-                                // setState(() {
-                                //   // cells[index] = true;
-                                // });
+                                
                               },
                               child: Container(
                                 margin: EdgeInsets.all(1.0),
                                 color: Color.fromARGB(10, 0, 0, 0),
                                 child: Center(
-                                  child: cells[index]['state'] == 0 ? null : cells[index]['state'] == 1 ? Text('X', style: TextStyle(color: Colors.red),) : Text('O', style: TextStyle(color: Colors.blue)),
+                                  child: cells[index]['state'] == 0 ? null : cells[index]['state'] == 1 ? Image.asset('assets/images/icons8-multiply-96.png', scale: 4.0,) : Image.asset('assets/images/icons8-circle-96.png', scale: 4.0,),
                                 ),
                               ),
                             );
